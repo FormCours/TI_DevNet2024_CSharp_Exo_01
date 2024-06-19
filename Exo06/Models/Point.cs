7f35f9a711526b1af7efc6361928e6173e0ded48
Aurélien <aurelien.strimelle@bstorm.be>
2024-06-19 17:22:40 +0200

    Exo Structure

---
+++ Exo06/Models/Point.cs
@@ -0,0 +1,8 @@
+﻿namespace Exo06.Models
+{
+    public struct Point
+    {
+        public int X;
+        public int Y;
+    }
+}
---
+++ Exo06/Program.cs
@@ -0,0 +1,16 @@
+﻿using Exo06.Models;
+
+int SIZE = 5;
+
+Point?[,] myPoints = new Point?[SIZE, SIZE];
+
+for (int i = 0; i < SIZE; i++)
+{
+    Point point;
+    point.X = i + 1;
+    point.Y = i + 1;
+
+    myPoints[i, i] = point;
+}
+
+
