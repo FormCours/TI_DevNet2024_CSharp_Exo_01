eb67935bf7dd6237da5db81627aac79504515ae5
Aurélien <aurelien.strimelle@bstorm.be>
2024-06-19 15:30:15 +0200

    Exo 05.3

--- Exo05/Program.cs
+++ Exo05/Program.cs
@@ -1,5 +1,8 @@
-﻿// Exo 02 - Grâce à une boucle « for » et à l’aide d’une collection générique, calculez les x premiers nombres premiers (version optimisée).
+﻿using System.Text;
 
+// Exo 02 - Grâce à une boucle « for » et à l’aide d’une collection générique, calculez les x premiers nombres premiers (version optimisée).
+
+/*
 {
     Console.Write("Nombre prémier désiré : ");
     int nbUserRequest = int.Parse(Console.ReadLine()!);
@@ -37,6 +40,7 @@
     Console.WriteLine($"Nombre premier : {string.Join(", ", primes)}");
     Console.WriteLine($"Nb operation pour trouver : {nbOp}");
 }
+*/
 
 // Pour info, nombre d'opération avec et sans la collection
 /*
@@ -52,3 +56,48 @@
  *    1 000 =>   25 134 op
  *   10 000 =>  684 044 op
  */
+
+// Exo 03 - Demandez à l’utilisateur d’introduire deux nombres au clavier et faite l’addition de ces deux nombres en ne convertissant que caractère par caractère. (Méthode « ToCharArray() » de la classe « string »).
+
+// Saisie utilisateur
+Console.WriteLine("Veuillez encoder les deux nombres a additionner : ");
+Console.Write(" > ");
+string val1 = Console.ReadLine()!;
+Console.Write(" > ");
+string val2 = Console.ReadLine()!;
+
+// Convertir les valeurs en tableau de char et inversé (pour les process)
+char[] nb1Reverse = val1.ToCharArray().Reverse().ToArray();
+char[] nb2Reverse = val2.ToCharArray().Reverse().ToArray();
+
+List<int> result = new List<int>();
+int report = 0;
+
+for(int i = 0; i < nb1Reverse.Length || i < nb2Reverse.Length;  i++)
+{
+    int elem1 = (i < nb1Reverse.Length) ? int.Parse(nb1Reverse[i].ToString()) : 0;
+    int elem2 = (i < nb2Reverse.Length) ? (nb2Reverse[i] - '0') : 0;
+    // NB: Le char '0' vaut la valeur 48 dans la table ASCII
+
+    // Addition des 2 nombres et du report si necessaire
+    int temp = elem1 + elem2 + report;
+
+    // Calcul du report et du resultat
+    report = temp / 10;
+    result.Insert(0, temp % 10);
+}
+if(report != 0)
+{
+    result.Insert(0, report);
+}
+
+StringBuilder sb =  new StringBuilder();
+foreach(int elem in result)
+{
+    sb.Append(elem);
+}
+string finalResult1 = sb.ToString();
+string finalResult2 = string.Join("", result);
+
+Console.WriteLine($"Resultat : {val1} + {val2} = {finalResult1}");
+
