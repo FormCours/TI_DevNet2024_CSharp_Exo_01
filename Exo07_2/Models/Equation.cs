7ce316cb8a5c87d641f5d46d589446463633dc83
studentdot0500 <studentdot0500@de.techni.dom>
2024-06-24 16:35:09 +0200

    Exo 07.2 - Equation

---
+++ Exo07_2/Models/Equation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo07_2.Models
+{
+    public struct Equation
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public bool Resoudre(out double? X1, out double? X2)
+        {
+            double delta = Math.Pow(B, 2) - 4 * A * C;
+            bool success = false;
+
+            X1 = null;
+            X2 = null;
+
+            if (delta >= 0)
+            {
+                X1 = (-B + Math.Sqrt(delta)) / (2 * A);
+                X2 = (-B - Math.Sqrt(delta)) / (2 * A);
+                success = true;
+            }
+
+            return success;
+        }
+    }
+}
---
+++ Exo07_2/Program.cs
@@ -0,0 +1,11 @@
+﻿using Exo07_2.Models;
+
+Equation equation = new Equation();
+
+equation.A = 2;
+equation.B = 9;
+equation.C = -5;
+
+equation.Resoudre(out double? X1, out double? X2);
+
+Console.WriteLine($"X1 ► {X1} , X2 ► {X2}");
