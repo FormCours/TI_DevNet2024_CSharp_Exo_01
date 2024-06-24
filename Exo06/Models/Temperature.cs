84d95bfb6cbd1f72ec986f9de4be16d4ac1fc1cd
studentdot0500 <studentdot0500@de.techni.dom>
2024-06-24 16:06:11 +0200

    Exo 06 - Suite

---
+++ Exo06/Models/Temperature.cs
@@ -0,0 +1,15 @@
+﻿namespace Exo06.Models
+{
+    // Ecrire deux structures Celsius et Fahrenheit toutes deux ayant une variable de type double appelée « Temperature ».
+
+    public struct Celsius
+    {
+        public double Temperature;
+    }
+
+    public struct Fahrenheit
+    {
+        public double Temperature;
+    }
+
+}
--- Exo06/Program.cs
+++ Exo06/Program.cs
@@ -14,3 +14,23 @@ for (int i = 0; i < SIZE; i++)
 }
 
 
+// La méthode "GetLength(index)" permet d'obtenir le nombre d'élément
+// dans une des dimensions du tableau matriciel
+Console.WriteLine($"Dim 0 : {myPoints.GetLength(0)} / Dim 1 : {myPoints.GetLength(1)} / Total : {myPoints.Length}");
+
+for(int x = 0; x < myPoints.GetLength(0); x++)
+{
+    for (int y = 0; y < myPoints.GetLength(1); y++)
+    {
+        if (myPoints[x, y] != null)
+        {
+            Point p = (Point)myPoints[x, y];
+
+            Console.CursorLeft = y * 12;
+            Console.Write($"(X:{p.X} - Y:{p.Y})");
+        }
+    }
+    Console.WriteLine();
+}
+
+
